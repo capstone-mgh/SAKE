e0a39a1c7112a2b84ba56843d8de886cdbc899a9
Shawn Pan <shawnpan@alum.mit.edu>
2017-04-12 00:05:35 -0400

    Adding preliminary version of backend -- contours not yet working

---
+++ sake.py
@@ -0,0 +1,80 @@
+import dicom
+import os
+import numpy as np
+from scipy.ndimage import gaussian_filter, label, find_objects, binary_propagation
+from skimage.measure import find_contours, points_in_poly
+
+
+class Sake:
+
+	def __init__(self, path):
+		self.imagePath = path
+		self.imageArray = None
+		self.loadSeries(path)
+
+	def loadSeries(self, path):
+		self.imagePath = path
+		# read from the URL. Right now this is local path
+
+		lstFilesDCM = []  # create an empty list
+		for dirName, subdirList, fileList in os.walk(self.imagePath):
+			for filename in fileList:
+				if ".dcm" in filename.lower():  # check whether the file's DICOM
+					lstFilesDCM.append(os.path.join(dirName,filename))
+								
+		# Get ref file
+		RefDs = dicom.read_file(lstFilesDCM[0])
+
+		# Load dimensions based on the number of rows, columns, and slices (along the Z axis)
+		ConstPixelDims = (len(lstFilesDCM), int(RefDs.Rows), int(RefDs.Columns))
+
+		# Load spacing values (in mm)
+		ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), float(RefDs.PixelSpacing[1]), 1.)
+
+		# The array is sized based on 'ConstPixelDims'
+		self.imagesArray = np.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
+
+		# loop through all the DICOM files
+		for filenameDCM in lstFilesDCM:
+			# read the file
+			ds = dicom.read_file(filenameDCM)
+			# store the raw image data
+			self.imagesArray[ds.InstanceNumber-1, :, :] = ds.pixel_array
+		
+		return self.imagesArray.shape
+
+	def getImage(self, z):
+		return self.imagesArray[z]
+
+	def output_mask(self, x, y, image, tolerance=0.1):
+		output = np.zeros_like(image)
+		input_mask = np.zeros_like(image)
+		input_mask[x,y] = 1
+		tolerance *= np.max(image)
+		greater = np.greater_equal(image,image[x,y]-tolerance)
+		less = np.less_equal(image,image[x,y]+tolerance)
+		return binary_propagation(input = input_mask, mask = greater*less)
+
+	def segmentImage(self, z, x, y, threshold=0.1, x_bound=64, y_bound=64, thinning=1):
+		x_offset = max(0, x - x_bound)
+		y_offset = max(0, y - y_bound)
+		x_bounding = slice(x_offset, min(self.imagesArray.shape[1], x + x_bound))
+		y_bounding = slice(y_offset, min(self.imagesArray.shape[2], y + y_bound))
+		bounding_slice = self.imagesArray[z, x_bounding, y_bounding]
+		new_x = x - x_offset
+		new_y = y - y_offset
+		segmented_slice = self.output_mask(new_x, new_y, bounding_slice, threshold)
+		contour_slice = find_contours(segmented_slice, level=0.5)
+
+		print "Debug"
+		print x, y, z
+		print x_offset, y_offset
+		print x_bounding, y_bounding
+		print self.imagesArray.shape
+		print bounding_slice.shape
+		print bounding_slice
+		print segmented_slice.shape
+		print segmented_slice
+		print contour_slice
+
+		return contour_slice[0][::thinning] + np.array([x_offset, y_offset])
--- segment.py
+++ segment.py
@@ -2,17 +2,38 @@
 #Flask rest endpoint for segmentation
 from flask import Flask, request
 import json
+from sake import Sake
 app = Flask(__name__)
 
-#dummy segment endpoint
+@app.route("/version")
+def index():
+        return "0.12"
+
 @app.route("/segment")
 def segment():
-  x = int(request.args.get("x", "0"))
-  y = int(request.args.get("y", "0"))
-  z = int(request.args.get("z", "0"))
-  stack = request.args.get("stack", "")
-  threshold = float(request.args.get("threshold", "0.5"))
-  return json.dumps([[x, y-7], [x+3, y-3], [x+4, y], [x+3, y+7], [x, y+7], [x-3, y+3], [x-4, y], [x-3, y-7]])
+        #x-position
+        x = int(request.args.get("x", "0"))
+        #y-position
+        y = int(request.args.get("y", "0"))
+        #0-based z-index
+        z = int(request.args.get("z", "0"))
+        #url to image
+        imageId = request.args.get("imageId", "")
+
+        #todo clean up frontend api
+        patientId = imageId.split("/")[3]
+        path = "/opt/bitnami/apache2/htdocs/" + patientId
+        print path
+
+        sake = Sake(path)
+        print "webparams", x, y, z
+        polygon = sake.segmentImage(z, x, y)
+        return json.dumps(polygon.tolist())
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Page not found", 404
 
 if __name__ == "__main__":
-  app.run()
+#       app.debug = True
+        app.run()
