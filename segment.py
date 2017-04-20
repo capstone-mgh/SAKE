fa0356ec4694811e0ab50db61fddc0c1c63694fd
Shawn Pan <shawnpan@alum.mit.edu>
2017-04-19 21:57:29 -0400

    Updating REST endpoint

--- segment.py
+++ segment.py
@@ -8,43 +8,55 @@ app = Flask(__name__)
 
 @app.route("/version")
 def index():
-        return "0.16"
+    return "0.17"
 
 @app.route("/segment")
 def segment():
-        #x-position
-        x = int(request.args.get("x", "0"))
-        #y-position
-        y = int(request.args.get("y", "0"))
-        #0-based z-index
-        z = int(request.args.get("z", "0"))
-        #url to image
-        imageId = request.args.get("imageId", "")
+    print "Request arguments"
+    print request.args
 
-        #todo clean up frontend api
-        patientId = imageId.split("/")[3]
-        path = "/opt/bitnami/apache2/htdocs/" + patientId
-        print path
+    #x-position
+    x = int(request.args.get("x", "0"))
+    #y-position
+    y = int(request.args.get("y", "0"))
+    #0-based z-index
+    z = int(request.args.get("z", "0"))
+    #clicked x-position
+    xOrig = int(request.args.get("xOrig", "0"))
+    #clicked y-position
+    yOrig = int(request.args.get("yOrig", "0"))
+    #clicked z-index
+    zOrig = int(request.args.get("zOrig", "0"))
+    #patient name
+    patientName = request.args.get("patientName", "")
+    #window width
+    windowWidth = int(request.args.get("windowWidth", 400))
+    #window center
+    windowCenter = int(request.args.get("windowCenter", 40))
+    #series id
+    seriesInstanceUid = request.args.get("seriesInstanceUid", "")
+    #instance id
+    sopInstanceUid = request.args.get("sopInstanceUid", "")
 
-        sake = Sake(path)
-        print "webparams", x, y, z
-        #mask, mask_offset, polygon = sake.segmentImage(z, x, y)
-        #TODO fix this coordinates hack
-        mask, mask_offset, polygon = sake.segmentImage(z, y, x)
-        mask = mask.T
-        mask_offset = np.array([mask_offset[1], mask_offset[0]])
-        polygon = np.column_stack((polygon[:,1], polygon[:,0]))
-        data = {
-                "mask": mask.tolist(),
-                "maskOffset": mask_offset.tolist(),
-                "polygon": polygon.tolist()
-        }
-        return json.dumps(data)
+    sake = Sake("/opt/bitnami/apache2/htdocs/" + patientName)
+    #mask, mask_offset, polygon = sake.segmentImage(z, x, y)
+    #TODO fix this coordinates hack
+    mask, mask_offset, polygon = sake.segmentImage(z, y, x)
+    mask = mask.T
+    mask_offset = np.array([mask_offset[1], mask_offset[0]])
+    polygon = np.column_stack((polygon[:,1], polygon[:,0]))
+    data = {
+            "mask": mask.tolist(),
+            "maskOffset": mask_offset.tolist(),
+            "polygon": polygon.tolist()
+    }
+    #frontend expects a json object with polygon and optionally mask + maskOffset
+    #returning a blank object or object with empty polygon will stop z-propagation
+    return json.dumps(data)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return "Page not found", 404
 
 if __name__ == "__main__":
-#       app.debug = True
-        app.run()
+    app.run()
